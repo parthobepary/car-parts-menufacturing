83c5932010a038a5299ea1ab90f9eaaba98efba8
pallobbiswas <pallobbiswas326@gmail.com>
2022-05-21 22:46:00 +0600

    navbar fnished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
-import Navbar from './pages/Shared/Navbar';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/Authentication/Login";
+import Ragister from "./pages/Authentication/Ragister";
+import Blogs from "./pages/Blogs/Blogs";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Home from "./pages/Home/Home";
+import Portfolio from "./pages/Portfolio/Portfolio";
+import Navbar from "./pages/Shared/Navbar";
 
 function App() {
   return (
-    <div className="App"  data-theme= "light" >
+    <div className="App" data-theme="light">
       <Navbar>
-
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/portfolio" element={<Portfolio />}></Route>
+          <Route path="/blogs" element={<Blogs />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/ragister" element={<Ragister />}></Route>
+        </Routes>
       </Navbar>
-
     </div>
   );
 }
